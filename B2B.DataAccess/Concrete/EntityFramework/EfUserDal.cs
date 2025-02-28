531cc7ba8fb9d754282fdf12e0958c31752cdec8
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-28 16:36:33 +0300

    generic yapı tamamlandı.

---
+++ B2B.DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,12 @@
+﻿
+
+using B2B.Core.DataAccess.EntityFramework;
+using B2B.Core.Entities.Concrete;
+using B2B.DataAccess.Abstract;
+using B2B.DataAccess.Concrete.EntityFramework.Context;
+
+namespace B2B.DataAccess.Concrete.EntityFramework;
+
+public class EfUserDal:EfEntityRepositoryBase<User,ContextDb>,IUserDal
+{
+}
