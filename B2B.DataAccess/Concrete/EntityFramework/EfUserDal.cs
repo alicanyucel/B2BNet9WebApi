d0120d75fdbf28549ea579db48e3dd81a4b89bd7
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-03-04 17:42:59 +0300

    clean code

--- B2B.DataAccess/Concrete/EntityFramework/EfUserDal.cs
+++ B2B.DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -3,26 +3,25 @@ using B2B.Core.Entities.Concrete;
 using B2B.DataAccess.Abstract;
 using B2B.DataAccess.Concrete.EntityFramework.Context;
 
-namespace B2B.DataAccess.Concrete.EntityFramework
-{
-    public class EfUserDal : EfEntityRepositoryBase<User, ContextDb>, IUserDal
-    {
-        public List<OperationClaim> GetClaims(User user, int companyId)
-        {
-            using (var context = new ContextDb())
-            {
-                var result = from operationClaim in context.OpetationClaims
-                             join userOperationClaim in context.UserOperatioClaims
-                             on operationClaim.Id equals userOperationClaim.OperationClaimId
-                             where userOperationClaim.CompanyId == companyId && userOperationClaim.UserId == user.Id
-                             select new OperationClaim
-                             {
-                                 Id = operationClaim.Id,
-                                 Name = operationClaim.Name
-                             };
+namespace B2B.DataAccess.Concrete.EntityFramework;
 
-                return result.ToList();
-            }
+public class EfUserDal : EfEntityRepositoryBase<User, ContextDb>, IUserDal
+{
+    public List<OperationClaim> GetClaims(User user, int companyId)
+    {
+        using (var context = new ContextDb())
+        {
+            var result = from operationClaim in context.OpetationClaims
+                         join userOperationClaim in context.UserOperatioClaims
+                         on operationClaim.Id equals userOperationClaim.OperationClaimId
+                         where userOperationClaim.CompanyId == companyId && userOperationClaim.UserId == user.Id
+                         select new OperationClaim
+                         {
+                             Id = operationClaim.Id,
+                             Name = operationClaim.Name
+                         };
+
+            return result.ToList();
         }
     }
 }
