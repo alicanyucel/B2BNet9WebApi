e24f309186ad05a2d786a3535b420ae71594b363
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-03-04 15:02:39 +0300

    yorum satırları kaldırıldı.

---
+++ B2B.Business/Concrete/AccountReconciliationManager.cs
@@ -0,0 +1,13 @@
+﻿using B2B.Business.Abstract;
+using B2B.DataAccess.Abstract;
+
+namespace B2B.Business.Concrete;
+
+public class AccountReconciliationManager:IAccountReconciliationService
+{
+    private readonly IAccountReconciliationDal _accountReconciliationDal; 
+    public AccountReconciliationManager(IAccountReconciliationDal accountReconciliationDal)
+    {
+        _accountReconciliationDal= accountReconciliationDal;
+    }
+}
--- B2B.WebApi/Program.cs
+++ B2B.WebApi/Program.cs
@@ -4,17 +4,13 @@ using B2B.Business.DependencyResolvers.Autofac;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacBusinessModule()));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
